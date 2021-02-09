206a1ddc535d4e525d018cafb54864291fce2875
hibban2306 <hahagokil4@gmail.com>
2021-02-09 14:37:29 +0700

    hari2

---
+++ 01-02/input_output.py
@@ -0,0 +1,14 @@
+nama = input("masukkan nama anda: ")
+print("\nhai" ,nama,"\n")
+
+
+bil1 = int(input('Masukan Bilangan ke-1 = '))
+bil2 = int(input('Masukan Bilangan ke-2 = '))
+
+print("\nDan Hasilnya Adalah..\n")
+
+tambah = bil1+bil2
+kali =  bil1*bil2
+print("Hasil tambahnya adalah = ", tambah)
+print("Hasilnya perkaliannya adalah = ", kali)
+
